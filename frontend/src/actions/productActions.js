41e6ad0507daec2355f9299a7dd798b4439db12c
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-10-01 15:48:43 -0700

    redux setup Homescreen useDispatch useSelector

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,28 @@
+//import Axios from "axios"
+import {
+	PRODUCT_LIST_FAIL,
+	PRODUCT_LIST_SUCCESS,
+	PRODUCT_LIST_REQUEST
+} from "../constants/productContants";
+import axios from "axios";
+
+export const listProducts = () => async dispatch => {
+	try {
+		dispatch({ type: PRODUCT_LIST_REQUEST });
+
+		const { data } = await axios.get("/api/products");
+
+		dispatch({
+			type: PRODUCT_LIST_SUCCESS,
+			payload: data
+		});
+	} catch (error) {
+		dispatch({
+			type: PRODUCT_LIST_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message
+		});
+	}
+};
