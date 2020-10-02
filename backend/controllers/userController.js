bf031d19e3163740b983e0ac15ccf8eea9759ff8
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-10-02 14:14:05 -0700

    login and logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
 // @desc		Auth user & get token
@@ -17,7 +18,7 @@ const authUser = asyncHandler(async (req, res) => {
 			name: user.name,
 			email: user.email,
 			isAdmin: user.isAdmin,
-			token: null
+			token: generateToken(user._id)
 		});
 	} else {
 		res.status(401);
@@ -25,4 +26,57 @@ const authUser = asyncHandler(async (req, res) => {
 	}
 });
 
-export { authUser };
+// @desc		Register a new user
+// @route	POST /api/users
+// @access	Public
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+	// res.send({ email, password });
+
+	const userExists = await User.findOne({ email: email });
+
+	if (userExists) {
+		res.status(400);
+		throw new Error("User already exists");
+	}
+	const user = await User.create({
+		name,
+		email,
+		password
+	});
+
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id)
+		});
+	} else {
+		res.status(400);
+		throw new Error("Invalid User Data");
+	}
+});
+
+// @desc		Get user profile
+// @route	POST /api/users/profile
+// @access	Private
+const getUserProfile = asyncHandler(async (req, res) => {
+	//res.send("success");
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin
+		});
+	} else {
+		res.status(404);
+		throw new Error("User not found");
+	}
+});
+
+export { authUser, getUserProfile, registerUser };
