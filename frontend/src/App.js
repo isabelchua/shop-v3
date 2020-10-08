0c14b4ab45cd482605ec8ba65b05e9bc9e625347
isabelchua <isabelchua86@gmail.com>
2020-10-08 13:27:27 -0700

    place order

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
 import { Container } from "react-bootstrap";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
@@ -14,15 +13,15 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 
-function App() {
+const App = () => {
 	return (
 		<Router>
 			<Header />
 			<main className="py-3">
 				<Container>
 					<Route path="/shipping" component={ShippingScreen} />
-					<Route path="/placeorder" component={PlaceOrderScreen} />
 					<Route path="/payment" component={PaymentScreen} />
+					<Route path="/placeorder" component={PlaceOrderScreen} />
 					<Route path="/login" component={LoginScreen} />
 					<Route path="/register" component={RegisterScreen} />
 					<Route path="/profile" component={ProfileScreen} />
@@ -34,6 +33,6 @@ function App() {
 			<Footer />
 		</Router>
 	);
-}
+};
 
 export default App;
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,159 +1,164 @@
-import axios from "axios";
+import axios from 'axios'
 import {
-	USER_LOGIN_REQUEST,
-	USER_LOGIN_SUCCESS,
-	USER_LOGIN_FAIL,
-	USER_LOGOUT,
-	USER_REGISTER_REQUEST,
-	USER_REGISTER_SUCCESS,
-	USER_REGISTER_FAIL,
-	USER_DETAILS_REQUEST,
-	USER_DETAILS_SUCCESS,
-	USER_DETAILS_FAIL,
-	USER_UPDATE_PROFILE_REQUEST,
-	USER_UPDATE_PROFILE_SUCCESS
-} from "../constants/userConstants";
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
+} from '../constants/userConstants'
 
-export const login = (email, password) => async dispatch => {
-	try {
-		dispatch({
-			type: USER_LOGIN_REQUEST
-		});
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    })
 
-		const config = {
-			headers: {
-				"Content-Type": "application/json"
-			}
-		};
-		const { data } = await axios.post(
-			"/api/users/login",
-			{ email, password },
-			config
-		);
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
 
-		dispatch({
-			type: USER_LOGIN_SUCCESS,
-			payload: data
-		});
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password },
+      config
+    )
 
-		localStorage.setItem("userInfo", JSON.stringify(data));
-	} catch (error) {
-		dispatch({
-			type: USER_LOGIN_FAIL,
-			payload:
-				error.response && error.response.data.message
-					? error.response.data.message
-					: error.message
-		});
-	}
-};
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    })
 
-export const logout = () => dispatch => {
-	localStorage.removeItem("userInfo");
-	dispatch({ type: USER_LOGOUT });
-};
+    localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
 
-export const register = (name, email, password) => async dispatch => {
-	try {
-		dispatch({
-			type: USER_REGISTER_REQUEST
-		});
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo')
+  dispatch({ type: USER_LOGOUT })
+}
 
-		const config = {
-			headers: {
-				"Content-Type": "application/json"
-			}
-		};
-		const { data } = await axios.post(
-			"/api/users",
-			{ name, email, password },
-			config
-		);
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    })
 
-		dispatch({
-			type: USER_REGISTER_SUCCESS,
-			payload: data
-		});
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
 
-		dispatch({
-			type: USER_LOGIN_SUCCESS,
-			payload: data
-		});
+    const { data } = await axios.post(
+      '/api/users',
+      { name, email, password },
+      config
+    )
 
-		localStorage.setItem("userInfo", JSON.stringify(data));
-	} catch (error) {
-		dispatch({
-			type: USER_REGISTER_FAIL,
-			payload:
-				error.response && error.response.data.message
-					? error.response.data.message
-					: error.message
-		});
-	}
-};
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    })
 
-export const getUserDetails = id => async (dispatch, getState) => {
-	try {
-		dispatch({
-			type: USER_DETAILS_REQUEST
-		});
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    })
 
-		const {
-			userLogin: { userInfo }
-		} = getState();
+    localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
 
-		const config = {
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${userInfo.token}`
-			}
-		};
-		const { data } = await axios.get(`/api/users/${id}`, config);
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST,
+    })
 
-		dispatch({
-			type: USER_DETAILS_SUCCESS,
-			payload: data
-		});
-	} catch (error) {
-		dispatch({
-			type: USER_DETAILS_FAIL,
-			payload:
-				error.response && error.response.data.message
-					? error.response.data.message
-					: error.message
-		});
-	}
-};
+    const {
+      userLogin: { userInfo },
+    } = getState()
 
-export const updateUserProfile = user => async (dispatch, getState) => {
-	try {
-		dispatch({
-			type: USER_UPDATE_PROFILE_REQUEST
-		});
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
 
-		const {
-			userLogin: { userInfo }
-		} = getState();
+    const { data } = await axios.get(`/api/users/${id}`, config)
 
-		const config = {
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${userInfo.token}`
-			}
-		};
-		const { data } = await axios.put(`/api/users/profile`, user, config);
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
 
-		dispatch({
-			type: USER_UPDATE_PROFILE_SUCCESS,
-			payload: data
-		});
-	} catch (error) {
-		dispatch({
-			type: USER_DETAILS_FAIL,
-			payload:
-				error.response && error.response.data.message
-					? error.response.data.message
-					: error.message
-		});
-	}
-};
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_UPDATE_PROFILE_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(`/api/users/profile`, user, config)
+
+    dispatch({
+      type: USER_UPDATE_PROFILE_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_PROFILE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
