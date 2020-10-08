1688f9c53e1a63b013d2b8442e719f9621f0d71b
isabelchua <isabelchua86@gmail.com>
2020-10-08 13:51:35 -0700

    order routes

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -8,6 +8,23 @@ import CheckoutSteps from "../components/CheckoutSteps";
 const PlaceOrderScreen = () => {
 	const cart = useSelector(state => state.cart);
 	//calculate price
+	const addDecimals = num => {
+		return (Math.round(num * 100) / 100).toFixed(2);
+	};
+
+	cart.itemsPrice = addDecimals(
+		cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+	);
+
+	cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100);
+
+	cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
+	cart.totalPrice = (
+		Number(cart.itemsPrice) +
+		Number(cart.shippingPrice) +
+		Number(cart.taxPrice)
+	).toFixed(2);
+
 	const placeOrderHandler = () => {
 		console.log("order");
 	};
@@ -57,53 +74,6 @@ const PlaceOrderScreen = () => {
 													{item.qty} x ${item.price} = $
 													{item.qty * item.price}
 												</Col>
-												<Col md={4}>
-													<Card>
-														<ListGroup variant="flush">
-															<ListGroup.Item>
-																<h2>Order Summary</h2>
-															</ListGroup.Item>
-															<ListGroup.Item>
-																<Row>
-																	<Col>Items</Col>
-																	<Col>${cart.itemsPrice}</Col>
-																</Row>
-															</ListGroup.Item>
-															<ListGroup.Item>
-																<Row>
-																	<Col>Shipping</Col>
-																	<Col>
-																		${cart.shippingPrice}
-																	</Col>
-																</Row>
-															</ListGroup.Item>
-															<ListGroup.Item>
-																<Row>
-																	<Col>Tax</Col>
-																	<Col>${cart.taxPrice}</Col>
-																</Row>
-															</ListGroup.Item>
-															<ListGroup.Item>
-																<Row>
-																	<Col>Total</Col>
-																	<Col>${cart.totalPrice}</Col>
-																</Row>
-															</ListGroup.Item>
-															<ListGroup.Item>
-																<Button
-																	type="button"
-																	className="btn-block"
-																	disabled={
-																		cart.cartItems === 0
-																	}
-																	onClick={placeOrderHandler}
-																>
-																	Place Order
-																</Button>
-															</ListGroup.Item>
-														</ListGroup>
-													</Card>
-												</Col>
 											</Row>
 										</ListGroup.Item>
 									))}
@@ -111,6 +81,49 @@ const PlaceOrderScreen = () => {
 							)}
 						</ListGroup.Item>
 					</ListGroup>
+				</Col>
+				<Col md={4}>
+					<Card>
+						<ListGroup variant="flush">
+							<ListGroup.Item>
+								<h2>Order Summary</h2>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Items</Col>
+									<Col>${cart.itemsPrice}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Shipping</Col>
+									<Col>${cart.shippingPrice}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Tax</Col>
+									<Col>${cart.taxPrice}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Total</Col>
+									<Col>${cart.totalPrice}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Button
+									type="button"
+									className="btn-block"
+									disabled={cart.cartItems === 0}
+									onClick={placeOrderHandler}
+								>
+									Place Order
+								</Button>
+							</ListGroup.Item>
+						</ListGroup>
+					</Card>
 				</Col>
 			</Row>
 		</>
